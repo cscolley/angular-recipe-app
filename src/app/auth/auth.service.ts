546444f32d2104fd9d21de6718df3bb077392c5f
ccolley <ccolley@bravurasolutions.com>
2022-02-07 09:44:35 +1100

    Complete part 300 of Scetion 20 Auth & Route Protection

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,94 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Subject, throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { User } from "./user.model";
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({ providedIn: "root" })
+export class AuthService {
+  user = new Subject<User>();
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA_JwauDXf-d6uju4XDRXzArASBMTXyz5M",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA_JwauDXf-d6uju4XDRXzArASBMTXyz5M",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let errorMessage = "An unknown error occurred!";
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorResponse.error.error.message) {
+      case "EMAIL_EXISTS":
+        errorMessage = "This email already exists!";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "This email does not exist!";
+        break;
+      case "INVALID_PASSWORD":
+        errorMessage = "This password is not correct!";
+        break;
+      default:
+        errorMessage = "An error occurred!";
+    }
+    return throwError(errorMessage);
+  }
+}
